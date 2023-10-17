9d02ea0515362940d63098966a8d7c14ee537cf4
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-17 17:44:40 +0530

    Create Find_Middle.py

---
+++ Find_Middle.py
@@ -0,0 +1,19 @@
+#This function will find the middle node of a singly linked list. If the list has an even number of nodes, they return the second middle node. If the list is empty, they return NULL
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
+def find_middle(head):
+    if head is None:
+        return None
+
+    slow = head
+    fast = head
+
+    while fast and fast.link:
+        slow = slow.link
+        fast = fast.link.link
+
+    return slow
